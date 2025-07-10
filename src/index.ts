1dc309f3556d771a1649f3877edbb5694b891d26
Li Lin <guru.lin@gmail.com>
2025-07-10 18:09:12 +0800

    update to use for 2 functions

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 export default {
 	async fetch(request: Request): Promise<Response> {
-		const cloudRunHostname = 'fund-analyst-1049830028293.us-central1.run.app';
 		const apiKey = 'n5aNKscfqByA6WE9xLGz7zBf7VzRuDQI';
 
 		const url = new URL(request.url);
-		url.hostname = cloudRunHostname;
+		if (url.hostname.startsWith("analyst")) {
+			url.hostname = 'fund-analyst-1049830028293.us-central1.run.app';
+		} else {
+			url.hostname = 'whoami-1049830028293.us-central1.run.app'
+		}
 
 		const newHeaders = new Headers(request.headers);
 		newHeaders.set('X-API-KEY', apiKey);
