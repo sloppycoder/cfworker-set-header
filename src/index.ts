314078482d3b24749d7542cdfcd5c6151e88e029
Li Lin <guru.lin@gmail.com>
2025-07-10 17:33:53 +0800

    update to real app

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 export default {
 	async fetch(request: Request): Promise<Response> {
-		const cloudRunHostname = 'whoami-1049830028293.us-central1.run.app';
+		const cloudRunHostname = 'fund-analyst-1049830028293.us-central1.run.app';
+		const apiKey = 'n5aNKscfqByA6WE9xLGz7zBf7VzRuDQI';
 
 		const url = new URL(request.url);
 		url.hostname = cloudRunHostname;
 
 		const newHeaders = new Headers(request.headers);
-		newHeaders.set('X-API-KEY', 'n5aNKscfqByA6WE9xLGz7zBf7VzRuDQI');
+		newHeaders.set('X-API-KEY', apiKey);
 
 		const modifiedRequest = new Request(url.toString(), {
 			method: request.method,
